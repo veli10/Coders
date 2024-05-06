1bd0a488af957cc976948e2b34528e100a5531a1
Ilhamz10 <ilhamzeynalov65@gmail.com>
2024-05-06 11:01:03 +0400

    task2

---
+++ task2.py
@@ -0,0 +1,68 @@
+# 1.) verilmiş listdəki ədədlərin hansılarının hər hansı bir ədədin kvadratı olduğunu define funksiyasında yazıb və listin içərisində ekrana çıxarın. 
+# import math
+
+# mylist=[-4,-16,0,1,4,5,9,16,25,36,49,64,81,100]
+# def listSqrt(list):
+#     sqrtNums = []
+#     for x in list:
+#         if x <= 0 or int(math.sqrt(x)) * int(math.sqrt(x)) != x: 
+#             continue
+#         else: 
+#             sqrtNums.append(int(math.sqrt(x)))
+
+#     return sqrtNums
+
+# print(listSqrt(mylist))
+
+
+#  2)Funksiya yazin list qebul etsin ve tekrarlanmayan elementleri bizə qaytarsın:
+#  input:[-1,1,2,2,6,7,7,'say']
+# a = [-1,1,2,2,6,7,7,'say']
+# def nonRepeative(myList):
+#     return [x for x in myList if myList.count(x) < 2]
+
+# print(nonRepeative(a))
+
+# 3) Verilmiş inputdakı bütün rəqəmlərin bir birlərinə hasilini icra edən funksiya yazın
+# a = input('Reqemler daxil edin?\n')
+# def getMultip(inp):
+#     sum = 1
+#     for a in inp.split():
+#         sum *= int(a)
+
+#     return sum
+
+# print(getMultip(a))
+
+# 4) verilmiş ədədin bölənlərini list comprehension istifadə edərək yazın
+# num = int(input('Eded daxil edin\n'))
+# print([x for x in range(1, num + 1) if num % x == 0])
+
+
+# 5)Əlininzdə ayların olduğu bir list var siz ay qarşısında uzunluğu olduğu bir dictionary yaradın və bunu comprehension ilə edin və alınan listi print etdirin.
+
+# mənim listim
+# mylist=['may','iyun','iyul']
+# bu şəkildə olacaq
+# {'may': 3, 'iyun': 4, 'iyul': 4}
+# mylist=['may','iyun','iyul']
+
+# print({x: len(x) for x in mylist})
+
+# 6)names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+#  verilmiş list-dən yalnız adların olduğu və kiçik hərflərlə yazıldığı list düzəldin və bunu conprehension ilə edin (əlavə olaraq funksiya da 
+# istifadə edəbilərsiz).
+# ['rick', 'morty', 'summer', 'jerry', 'beth']
+# names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+# print([x.split()[0].lower() for x in names])
+
+# 7) verilmiş iki listdəki ədədlərin indexlərinə uyğun olaraq ortalamasını tapın.
+# nums1 = [1, 2, 3]
+# nums2 = [4, 5, 6]
+
+# results = []
+# for i in range(0, len(nums1)):
+#     results.append((nums1[i] + nums2[i]) / 2)
+
+# print(results)
+# results=[ 2.5, 3.5, 4.5]
--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 # 1). Userden alinan 5 cavabi bir listde sirali seklinde yerlesdirin:
 # Userden alinan 5 cavabi bir listde sirali seklinde yerlesdirin:
 # [4,1,78,99,45] >> [1,4,45,78,99]
-numbers = []
-for i in range(0, 5):
-    number = int(input('Eded daxil edin: '))
-    numbers.append(number)
-numbers.sort()
-print(numbers)
+# numbers = []
+# for i in range(0, 5):
+#     number = int(input('Eded daxil edin: '))
+#     numbers.append(number)
+# numbers.sort()
+# print(numbers)
 
 
 
 # 2). Daxil edilmiş cümlənin bütün sözlərini alfabetik düzülüşlü sözlərə çevirib nəticəni çap edin. Misal üçün ."sabahin xeyir". 
 #  Bu şəkildə olacaq  : "abhins exiry"    . Hər bir sözdəki hərflər alfabetik sırasına görə düzüldü. 
-sentence = input('Cumle daxil edin: ')
-allWords = sentence.split(' ')
-answer = ''
-for word in allWords: 
-    sortedWord = ''.join(sorted(word))
-    answer += sortedWord + ' '
-print(answer)
+# sentence = input('Cumle daxil edin: ')
+# allWords = sentence.split(' ')
+# answer = ''
 
+# for word in allWords: 
+#     sortedWord = ''.join(sorted(word))
+#     answer += sortedWord + ' '
+# print(answer)
 
 
 # 3.)Tutaq k, n=13. istifadəçi bu n ededini inputda  daxil edənə qədər input alın ve 13 daxil etdikdə desin ki, məsələn 5 cəhdə tapdız, yəni, neçə cəhdə 
@@ -30,30 +30,34 @@ print(answer)
 # dorduncu input == 13
 
 # tebrikler . 4 cehdde 13 reqemeni tapdiz
-n = 13
-tries = 0
-while True:
-    number = int(input('Eded daxil edin: '))
-    if(n > number):
-        print('Daxil etdiyiniz eded asagidi, bextiniz birde sinayin')
-        tries += 1
-        continue
+# n = 13
+# tries = 0
+# while True:
+#     number = int(input('Eded daxil edin: '))
+#     if(n > number):
+#         print('Daxil etdiyiniz eded asagidi, bextiniz birde sinayin')
+#         tries += 1
+#         continue
     
-    elif(n < number):
-        print('Daxil etdiyiniz eded yuxaridi, bextiniz birde sinayin')
-        tries += 1
-        continue
+#     elif(n < number):
+#         print('Daxil etdiyiniz eded yuxaridi, bextiniz birde sinayin')
+#         tries += 1
+#         continue
 
-    else: 
-        tries += 1
-        print(f'Tebrikler, ededi {tries} cehdde tapdiniz')
-        break
+#     else: 
+#         tries += 1
+#         print(f'Tebrikler, ededi {tries} cehdde tapdiniz')
+#         break
 
 
 
-# 4). 1 ile 100 arasinda sade ededleri print edin. (for loops)
-for num in range(2, 100):
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            break
-    else: print(num)
+# # 4). 1 ile 100 arasinda sade ededleri print edin. (for loops)
+# for num in range(2, 100):
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             break
+#     else: print(num)
+
+nums = [1, 2, 3, 4, 5, 6]
+
+print([x * 2 for x in nums if x * 2 > 5])
